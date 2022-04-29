1efa98f791b03270149dc4f7725a835bcb4febb0
Jonny Rojas del Río <jonnyr@nserio.com>
2022-04-29 18:59:02 -0500

    create csvfile class

---
+++ ApiProject/ChallengeIdentidadTechnologies/ChallengeIdentidadTechnologies.Entities/POCOs/CsvFile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ChallengeIdentidadTechnologies.Entities.POCOs
+{
+	public class CsvFile
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public DateTime CreatedAt { get; set; }
+	}
+}

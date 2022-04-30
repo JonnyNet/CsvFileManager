0d5fbacfe8e11fd61287e7ad86532b199500d5a8
Jonny Rojas del Río <jonnyr@nserio.com>
2022-04-29 22:42:33 -0500

    repository project dependency injection

---
+++ ApiProject/ChallengeIdentidadTechnologies/ChallengeIdentidadTechnologies.Repository/DependencyContainer.cs
@@ -0,0 +1,33 @@
+﻿using ChallengeIdentidadTechnologies.Entities.Interfaces;
+using ChallengeIdentidadTechnologies.Repository.DataContext;
+using ChallengeIdentidadTechnologies.Repository.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace ChallengeIdentidadTechnologies.Repository
+{
+	public static class DependencyContainer
+	{
+		public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddDbContext<IdentidadTechnologiesContext>(options
+				=> options.UseSqlServer(configuration.GetConnectionString("IdentidadTechnologies")));
+			services.AddScoped<ICsvFileRepository, CsvFileRespository>();
+			services.AddScoped<IUnitOfWork, UnitOfWork>();
+			return services;
+		}
+
+		public static IApplicationBuilder RunMigrationsRepositories(this IApplicationBuilder app)
+		{
+			using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+			{
+				var context = serviceScope.ServiceProvider.GetRequiredService<IdentidadTechnologiesContext>();
+				context.Database.Migrate();
+			}
+			return app;
+		}
+	}
+}

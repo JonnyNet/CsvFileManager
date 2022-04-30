4f86daa2c2e75f99c3e714143231bba9cefff647
Jonny Rojas del Río <jonnyr@nserio.com>
2022-04-29 21:58:25 -0500

    created the migrations

--- ApiProject/ChallengeIdentidadTechnologies/ChallengeIdentidadTechnologies.Entities/POCOs/CsvFile.cs
+++ ApiProject/ChallengeIdentidadTechnologies/ChallengeIdentidadTechnologies.Entities/POCOs/CsvFile.cs
@@ -6,6 +6,7 @@ namespace ChallengeIdentidadTechnologies.Entities.POCOs
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
+		public string Table { get; set; }
 		public DateTime CreatedAt { get; set; }
 	}
 }
---
+++ ApiProject/ChallengeIdentidadTechnologies/ChallengeIdentidadTechnologies.Repository/DataContext/IdentidadTechnologiesContext.cs
@@ -0,0 +1,33 @@
+﻿using ChallengeIdentidadTechnologies.Entities.POCOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChallengeIdentidadTechnologies.Repository.DataContext
+{
+	public class IdentidadTechnologiesContext :  DbContext
+	{
+		public IdentidadTechnologiesContext(DbContextOptions<IdentidadTechnologiesContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<CsvFile> CsvFiles { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			var entityTypeBuilder = builder.Entity<CsvFile>();
+			entityTypeBuilder.HasKey(x => x.Id);
+
+			entityTypeBuilder.Property(x => x.Name)
+				.HasMaxLength(100)
+				.IsRequired();
+
+			entityTypeBuilder.Property(x => x.Table)
+				.HasMaxLength(50)
+				.IsRequired();
+
+			entityTypeBuilder.Property(x => x.CreatedAt)
+				.HasColumnType("datetime")
+				.HasDefaultValueSql("getdate()");
+		}
+	}
+}

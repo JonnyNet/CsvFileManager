4cbd52385b02c1df97ee0956c5b78767234037b1
Jonny Rojas del Río <jonnyr@nserio.com>
2022-04-29 19:07:56 -0500

    finalized CSV abstractions

---
+++ ApiProject/ChallengeIdentidadTechnologies/ChallengeIdentidadTechnologies.Entities/Interfaces/ICsvFileRepository.cs
@@ -0,0 +1,13 @@
+﻿using ChallengeIdentidadTechnologies.Entities.POCOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ChallengeIdentidadTechnologies.Entities.Interfaces
+{
+	public interface ICsvFileRepository
+	{
+		Task<int> Create(CsvFile csvFile);
+		Task<IEnumerable<CsvFile>> GetAll();
+		Task<CsvFile> Find(int id);
+	}
+}
